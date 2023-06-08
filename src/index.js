c0ef39e981b0a37d5780c4a939ffc5ee8a51e457
priyansh3731 <118509082+priyansh3731@users.noreply.github.com>
2023-06-07 22:41:56 -0700

    fix some warning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
@@ -77,9 +77,9 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
